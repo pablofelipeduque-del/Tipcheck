af5b3a2117aac7341f465485df49693effaffc23
pablofelipeduque-del <pablofelipeduque@gmail.com>
2026-05-03 20:58:37 -0700

    Fix geolocate to use HTTPS endpoint

--- app/api/geolocate/route.js
+++ app/api/geolocate/route.js
@@ -2,14 +2,14 @@ export async function GET(request) {
   const ip =
     request.headers.get('x-forwarded-for')?.split(',')[0] ||
     request.headers.get('x-real-ip') ||
-    '8.8.8.8';
+    '';
 
-  const res = await fetch(`http://ip-api.com/json/${ip}`);
+  const res = await fetch(`https://ipapi.co/${ip}/json/`);
   const data = await res.json();
 
   return Response.json({
-    lat: data.lat,
-    lng: data.lon,
+    lat: data.latitude,
+    lng: data.longitude,
     city: data.city,
   });
 }
